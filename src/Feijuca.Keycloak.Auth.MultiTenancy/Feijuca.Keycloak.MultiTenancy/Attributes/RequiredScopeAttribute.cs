7f73e5097db80cc4b1ec552e802b11feeac7407f
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2024-07-10 14:36:22 +0200

    FEAT: Adding new attribute to handle scopes

---
+++ src/Feijuca.Keycloak.Auth.MultiTenancy/Feijuca.Keycloak.MultiTenancy/Attributes/RequiredScopeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Feijuca.Keycloak.MultiTenancy.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class RequiredScopeAttribute(string scope) : Attribute, IAuthorizationFilter
+    {
+        private readonly string _scope = scope;
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+
+            var scopeClaim = user.FindFirst("scope");
+            if (scopeClaim == null || !scopeClaim.Value.Split(' ').Contains(_scope))
+            {
+                throw new UnauthorizedAccessException($"Scope {_scope} not attribuited on user {user!.Identity!.Name}.");
+            }
+        }
+    }
+}
